bde410a2b05c6b3de9ca00a9f6cb7f31df8cdf00
Quert <a97041304@gmail.com>
2019-08-29 12:58:40 +0800

    Update python_100_pc.py

--- python_100_pc.py
+++ python_100_pc.py
@@ -516,11 +516,27 @@ if __name__ == '__main__':
     a.sort()
     print(a)
 
-
-
-
-
-
+# 041
+    
+def varfunc():
+    var = 0
+    print('var = {}'.format(var))
+    var += 1
+    
+if __name__ == '__main__':
+    for i in range(3):
+        varfunc()
+    
+class Static:
+    StaticVar = 5
+    def varfunc(self):
+        self.StaticVar += 1
+        print(self.StaticVar)
+        
+print(Static.StaticVar)
+a = Static()
+for i in range(3):
+    a.varfunc()
 
 
 
